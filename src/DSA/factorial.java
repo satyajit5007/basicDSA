538373f462976846ab1925dfcb53eb725db7dbb1
Satyajit Bhosale <125465151+satyajit5007@users.noreply.github.com>
2025-08-19 22:44:01 +0530

    factorial

--- src/DSA/factorial.java
+++ src/DSA/factorial.java
@@ -1,5 +1,6 @@
 package DSA;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class factorial {
@@ -7,14 +8,25 @@ public class factorial {
         Scanner sc = new Scanner(System.in);
         System.out.println("how many checks");
         int t = sc.nextInt();
-        while (t-- >0){
-            System.out.println("enter the number");
+//        while (t-- >0){
+//            System.out.println("enter the number");
+//            int num = sc.nextInt();
+//            long factroial = 1;
+//            for(int i = 1 ; i<= num ; i++){
+//                factroial=factroial*i;
+//            }
+//            System.out.println("factorial of num "+ factroial);
+//        }
+
+        while (t-- > 0){
+            System.out.println("Enter the number");
             int num = sc.nextInt();
-            long factroial = 1;
-            for(int i = 1 ; i<= num ; i++){
-                factroial=factroial*i;
+            BigInteger factroial = BigInteger.ONE;
+            for (int i = num; i>= 1; i--){
+                factroial = factroial.multiply(BigInteger.valueOf(i));;
             }
-            System.out.println("factorial of num "+ factroial);
+            System.out.println("factroial of number "+ factroial);
         }
+
     }
 }
